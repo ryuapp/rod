67fb01a74918f598a118465ede6faa72a73cd95c
ryu <114303361+ryuapp@users.noreply.github.com>
2025-01-23 01:35:00 +0900

    bench: add just routing bench

--- benchmarks/simeple_bench.ts
+++ benchmarks/simeple_bench.ts
@@ -7,10 +7,19 @@ const frameworks = [
 ];
 
 frameworks.forEach(({ name, instance }) => {
-  Deno.bench(name, () => {
+  Deno.bench({ name, group: "simple-run" }, () => {
     const app = instance();
     // @ts-ignore: Unreachable code error
     app.get("/", () => new Response("Hello World"));
+    app.fetch(new Request("http://localhost"));
+  });
+});
+
+frameworks.forEach(({ name, instance }) => {
+  const app = instance();
+  // @ts-ignore: Unreachable code error
+  app.get("/", () => new Response("Hello World"));
+  Deno.bench({ name, group: "simple-just-routing" }, () => {
     app.fetch(new Request("http://localhost"));
   });
 });
