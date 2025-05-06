0bdd32be22ca160ae893553feb964ab9285d9efa
ryu <114303361+ryuapp@users.noreply.github.com>
2025-05-06 17:41:19 +0900

    bench: add Elysia

--- benchmarks/frameworks.ts
+++ benchmarks/frameworks.ts
@@ -3,11 +3,13 @@ import { Hono as Quick } from "@hono/hono/quick";
 import { Hono as Tiny } from "@hono/hono/tiny";
 import { Rod } from "@rod/rod";
 import { RawRod } from "../packages/rod/core.ts";
+import { Elysia } from "elysia";
 
 export const frameworks = [
   { name: "Hono", instance: () => new Hono() },
   { name: "Hono - quick preset", instance: () => new Quick() },
   { name: "Hono - tiny preset", instance: () => new Tiny() },
+  { name: "Elysia", instance: () => new Elysia() },
   { name: "Rod", instance: () => new Rod() },
   { name: "RawRod", instance: () => new RawRod() },
 ];
